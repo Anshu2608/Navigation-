424f96b853055d6332566f65b08c1a0e904a34a9
Anshu2608 <40367206+Anshu2608@users.noreply.github.com>
2024-06-24 14:53:11 +0530

    main.dart

--- main.dart
+++ main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:navig_route/first_screen.dart';
+import 'package:navig_route/second_screen.dart';
+
 
 void main() {
   runApp(
-   const MaterialApp(
-      title: 'Named Routes Demo',
-      home: FirstScreen(),
+    MaterialApp(
+      title: 'Named Route Example',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FirstScreen(),
+        '/second': (context) => const SecondScreen(),
+      },
       debugShowCheckedModeBanner: false,
     ),
   );
 }
+
+
+
+
 
 
